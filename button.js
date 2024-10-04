3229a2d72f5a672a43f39ade48232cf4bfb965cf
Jeffy2600II <nontakorn2600@gmail.com>
2024-10-04 07:53:54 +0700

    update uu

---
+++ button.js
@@ -0,0 +1,22 @@
+// เลือกปุ่มทั้งหมดที่มี name="button"
+const buttons = document.getElementsByName('button');
+
+// เพิ่ม event listener ให้กับแต่ละปุ่ม
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    // ดึงค่า href จากปุ่ม
+    const href = button.getAttribute('href');
+
+    // ตรวจสอบว่า href มีค่าหรือไม่
+    if (href) {
+      // เปิดหน้าเว็บใหม่ตามค่า href
+      window.location.href = href;
+    }
+  });
+});
+
+const backButton = document.querySelector('.back-button');
+
+  backButton.addEventListener('click', () => {
+    window.history.back();
+  });
